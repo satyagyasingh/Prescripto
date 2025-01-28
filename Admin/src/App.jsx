6ead4d42d5888902af606fc8bc68b9d974e8237c
satyagyasingh <satyagyasingh@gmail.com>
2025-01-28 22:50:05 +0530

    Admin panel setup

---
+++ Admin/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className='border-y-black border-x-purple-600 border-8 max-w-full '>Welcome</div>
+    </>
+  )
+}
+
+export default App
